ea41540e2c62a716cbfc17e8acb4bd7b02da8085
osteron <49269998+osteron@users.noreply.github.com>
2021-10-31 01:03:55 +0300

    Update прикол.py

--- прикол.py
+++ прикол.py
@@ -13,18 +13,18 @@ end = input("Введите конечную остановку: ")
 total = 0
 
 if start and end in new_tramway_line.keys():
-    begin_cicle = new_tramway_line[start][0]
+    begin_cicle = new_tramway_line[start][0] + 1
     end_cicle = new_tramway_line[end][0]
 
     if new_tramway_line[start][0] < new_tramway_line[end][0]:
         # Прямое направление
-        for i in range(begin_cicle+1, end_cicle+1):
+        for i in range(begin_cicle, end_cicle+1):
             for key, value in new_tramway_line.values():
                 if i == key:
                     total += value
     else:
         # Обратное направление
-        for i in range(end_cicle, begin_cicle+1):
+        for i in range(end_cicle, begin_cicle):
             for key, value in new_tramway_line.values():
                 if i == key:
                     total += value
