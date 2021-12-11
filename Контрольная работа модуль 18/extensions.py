8a30704b4f2b10f36598f780cc9c1ef15aec778b
osteron <49269998+osteron@users.noreply.github.com>
2021-12-11 23:46:00 +0300

    Update extensions.py

--- Контрольная работа модуль 18/extensions.py
+++ Контрольная работа модуль 18/extensions.py
@@ -7,14 +7,9 @@ class APIException(Exception):
 
 class CryptoConvertor:
     @staticmethod
-    def get_price(quote: str, base: str, amount: str):
-        if quote == base:
-            raise APIException(f'Невозможно перевести одинаковые валюты "{base}"')
-
-        try:
-            quote_ticker = keys[quote]
-        except KeyError:
-            raise APIException(f'Не удалось обработать валюту "{quote}"')
+    def get_price(base: str, quote: str, amount: str):
+        if base == quote:
+            raise APIException(f'Невозможно перевести одинаковые валюты "{quote}"')
 
         try:
             base_ticker = keys[base]
@@ -22,10 +17,15 @@ class CryptoConvertor:
             raise APIException(f'Не удалось обработать валюту "{base}"')
 
         try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту "{quote}"')
+
+        try:
             amount = float(amount)
         except ValueError:
             raise APIException(f'Не удалось обработать количество "{amount}"')
 
-        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-        total_base = json.loads(r.content)[keys[base]]
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
+        total_base = json.loads(r.content)[keys[quote]]
         return total_base
